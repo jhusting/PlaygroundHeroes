0a3f12ec739fda4c2a4b4c3b0727ef0e103ac27b
jhusting <joshhusting@gmail.com>
2019-01-20 00:46:32 -0800

    Archer imported. Working on logic now

---
+++ Source/PlaygroundHeroes/JArcher.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "JHero.h"
+#include "JArcher.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYGROUNDHEROES_API AJArcher : public AJHero
+{
+	GENERATED_BODY()
+	
+};
