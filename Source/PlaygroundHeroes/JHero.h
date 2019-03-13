f1bcd4c2dde6952cba4143ff7d71b855ddc27dd4
Theo Erickson <theoserickson@gmail.com>
2019-03-13 16:09:29 -0700

    Reverted JHero Stamina Change

--- Source/PlaygroundHeroes/JHero.h
+++ Source/PlaygroundHeroes/JHero.h
@@ -25,14 +25,6 @@ public:
 	// Sets default values for this character's properties
 	AJHero();
 
-	//If you want the player to move to a location upon level change
-	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Movement")
-	static bool MoveToNewLocationOnPlay;
-
-	//Vector 3 intended for use to keep a desired new location, static makes it persistent through level changes
-	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Movement")
-	static FVector LocationToMoveTo;
-
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseTurnRate;
@@ -46,7 +38,12 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
 	bool interacting;
-	
+
+	FORCEINLINE float GetMaxStamina() const { return MaxStamina; }
+
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	float SetMaxStamina(float x);
+
 	FORCEINLINE bool GetAttacking() const { return bAttacking; }
 
 	FORCEINLINE bool GetDodging() const { return bDodging; }
@@ -250,10 +247,10 @@ protected:
 	/*
 		The amount of stamina generated every second
 	*/
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	float StaminaGen;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	float AttackCost;
 
 	/*
